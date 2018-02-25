c432e6d789d4ba80a2f591d7476723a3f58065e9
Malcolm Davis <me@malcolmdavis.xyz>
2018-02-25 09:52:10 -0600

    File I/O idea dropped to simplify the problem.

--- Arquitectura_de_Computadores_II/Workshops/T1/saxpy.c
+++ Arquitectura_de_Computadores_II/Workshops/T1/saxpy.c
@@ -20,70 +20,68 @@
 #define VECTOR_SIZE 1000
 
 
-void generateFloatVector(int size, float* vector);
-void printFloatVector(int size, float* vector, int writeFile, const char* fileName);
-void getDataFromFile(const char* fileName, float* alphas,float* vectors, int* sizes, int * amount);
-void SAXPY(int size, float alpha, float* X, float* Y);
+void generateFloatVector(long size, float* vector);
+void printFloatVector(long size, float* vector);
+void SAXPY(long size, float alpha, float* X, float* Y);
 
 /*
  * Main method, retrive command line options, create the threads
  */
 int main(int argc, char const *argv[])
 {
-	const char* fileName = getenv("TMPFILE");
-	const int writeFile = getenv("WRITE_FILE") ? 1 : 0;
-	const int readFile = getenv("READ_FILE") ? 1 : 0;
+	const int printVectors = getenv("PRINT_VECTORS") ? 1 : 0;
 	double start_time, run_time;
 	srand(time(NULL));
-	if (readFile){
-		float *alphas;
-		float *vectors;
-		int* sizes;
-		int amount=0;
-		int ofset=0;
-		// Retrive all the vectors, alphas and sizes from the file
-		getDataFromFile(fileName, alphas, vectors, sizes, &amount);
-		// For every X, Y do the SAXPY
-		for(int i=0; i<amount/2; i++){
-			float *X = vectors+ofset;
-			float *Y = vectors+ofset+sizes[i];
-			float alpha = alphas[i];
-			start_time = omp_get_wtime();
-			SAXPY(sizes[i], alpha, X, Y);
-			run_time = omp_get_wtime() - start_time;
-			printFloatVector(sizes[i], Y, 0, fileName);
-		}
-		free(alphas);
-		free(vectors);
-		free(sizes);
-	}else{
-		int size = argc > 1 && atoi(argv[1]) > 0 ? atoi(argv[1]) : VECTOR_SIZE;
-		float alpha = ((float)rand()/(float)(RAND_MAX)) * FLOAT_RAND_MAX;
-		float* X = (float*)malloc(sizeof(float)*size);
-		float* Y = (float*)malloc(sizeof(float)*size);
-		printFloatVector(size, Y, 0, fileName);
-		printFloatVector(size, Y, 0, fileName);
-		start_time = omp_get_wtime();
-		SAXPY(size, alpha, X, Y);
-		run_time = omp_get_wtime() - start_time;
-		printFloatVector(size, Y, 0, fileName);
-		free(Y);
-		free(X);
+	// If the vector size is inserted then use it if not then use the default 
+	long size = argc > 1 && atol(argv[1]) > 0 ? atol(argv[1]) : VECTOR_SIZE;
+	float alpha = ((float)rand()/(float)(RAND_MAX)) * FLOAT_RAND_MAX;
+	// Allocate memmory for the vectors
+	float* X = (float*)malloc(sizeof(float)*size);
+	float* Y = (float*)malloc(sizeof(float)*size);
+	// Generate random vectors
+	generateFloatVector(size, X);
+	generateFloatVector(size, Y);
+	// Print the vectors
+	if(printVectors){
+		printf("----Y=aX+Y----\na: %f\n", alpha);
+		printf("X: ");
+		printFloatVector(size, X);
+		printf("Y: ");
+		printFloatVector(size, Y);
 	}
+	//Do the actual SAXPY and take the time
+	start_time = omp_get_wtime();
+	SAXPY(size, alpha, X, Y);
+	run_time = omp_get_wtime() - start_time;
+	//Print the result
+	if(printVectors){
+		printf("Y: ");
+		printFloatVector(size, Y);
+	}
+	printf("Size: %ld Seconds: %f \n", size, run_time);
+	// Free the allocated memmory
+	free(Y);
+	free(X);
 	return 0;
 }
 
 
-
-void SAXPY(int size, float alpha, float* X, float* Y){
-	
+/*
+ * SAXPY Function Y = aX + Y
+ * @param size the lenght of the vectors
+ * @param alpha the const to scale the vector X
+ * @param X a vector of floats
+ * @param Y a vector of floats
+ */
+void SAXPY(long size, float alpha, float* X, float* Y){
+	long i;
+	#ifdef PARALLEL
+	#pragma omp parallel for  private(i) shared(size, alpha, X, Y)
+	#endif
+	for(i = 0; i<size; i++){
+		Y[i]=alpha*X[i] + Y[i];
+	}
 }
-
-
-void getDataFromFile(const char* fileName, float* alphas,float* vectors, int* sizes, int * amount){
-	
-}
-
 
 
 /*
@@ -91,26 +89,21 @@ void getDataFromFile(const char* fileName, float* alphas,float* vectors, int* si
  * @param (INPUT)size the length of the vector
  * @param (OUTPUT)vector the place where the data will be stored.  
  */
-void generateFloatVector(int size, float* vector){
-	for(int i=0; i<size;i++){
+void generateFloatVector(long size, float* vector){
+	for(long i=0; i<size;i++){
 		vector[i] = ((float)rand()/(float)(RAND_MAX)) * FLOAT_RAND_MAX;
 	}
 }
 
 /*
- * Function that prints a vector on screen and in file 
+ * Function that prints a vector on screen
  * @param (INPUT)size the length of the vector
  * @param (INPUT)vector the place where the data will be stored. 
  */
-void printFloatVector(int size, float* vector, int writeFile, const char* fileName){
-	printf("\n[");
-	char buffer[size];
-	for(int i=0; i<size;i++){
+void printFloatVector(long size, float* vector){
+	printf("[");
+	for(long i=0; i<size;i++){
 		printf(" %f ", vector[i]);
-		if (writeFile) sprintf(buffer, " %f", vector[i]);
 	}
 	printf("]\n");
-	if (writeFile){
-		// Write buffer in file
-	}
 }
