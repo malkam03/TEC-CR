730b3c9cd726e8d549c5d6561c408bac2b215248
Malcolm Davis <me@malcolmdavis.xyz>
2018-02-25 09:48:00 -0600

    Dynamic labels for the plot.

--- Arquitectura_de_Computadores_II/Workshops/T1/plot.py
+++ Arquitectura_de_Computadores_II/Workshops/T1/plot.py
@@ -9,15 +9,20 @@ def main():
     data = sys.stdin.readlines()
     X = []
     Y = []
+    xLabel=""
+    yLabel=""
     for line in data:
         lineData = line.split(" ")
         if len(lineData) == 5:
             X.append(float(lineData[3]))
             Y.append(int(lineData[1]))
+            if xLabel == "":
+                yLabel = lineData[0]
+                xLabel = lineData[2]
     plt.plot(X, Y, 'ro')
     plt.axis([0, max(X), 0, max(Y)])
-    plt.ylabel("Steps")
-    plt.xlabel("time(s)")
+    plt.ylabel(yLabel)
+    plt.xlabel(xLabel)
     plt.show()
     
 
